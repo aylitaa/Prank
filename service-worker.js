461051f05ccd239e9042ddcf687f28f548719b3e
aylitaa <sadornotsad@gmail.com>
2024-06-02 13:45:16 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,8 @@
 self.addEventListener('notificationclick', function(event) {
-    var gifUrl = event.notification.data.url;
+    var gifUrl = event.notification.data.url; // Récupère l'URL du GIF à partir des données de la notification
     event.notification.close(); // Ferme la notification
     // Redirige l'utilisateur vers la page avec le GIF
     event.waitUntil(
-        clients.openWindow(gifUrl)
+        clients.openWindow(gifUrl) // Ouvre une nouvelle fenêtre avec l'URL du GIF
     );
 });
-
